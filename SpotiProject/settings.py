360c327a5a7a01c084356c8f1a24e5d1606143d3
emilyprietob <emilyprietob@gmail.com>
2024-11-29 12:06:34 -0400

    HTML and CSS for the login page

--- SpotiProject/settings.py
+++ SpotiProject/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
