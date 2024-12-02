b0dddc19236441901d89102bb5d639c539e91a4b
Benjamin Yohros <byohros6@gatech.edu>
2024-12-01 19:01:27 -0500

    Delete test_env.py
    Not needed

--- test_env.py
+++
@@ -1,5 +0,0 @@
-# test_env.py
-from decouple import config
-
-print("DJANGO_SECRET_KEY:", config("DJANGO_SECRET_KEY", default="NOT FOUND"))
-print("SPOTIFY_REDIRECT_URI:", config("SPOTIFY_REDIRECT_URI", default="NOT FOUND"))
